278283fbd06e7f3136c5c2118cb742e5f5cf1235
Chris Laidler <cjjlaidler@gmail.com>
2021-02-21 19:29:43 +0000

    add react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,38 @@
 import * as React from "react";
+import { Helmet } from "react-helmet";
 import logo from "../images/sw_logotype.svg";
 import joel from "../images/joel.png";
+import favicon from "../images/sw_icon-white.png";
 
 import styles from "./index.module.scss";
 
 const IndexPage = () => {
   return (
-
-
-    <div className={styles.page}>
-
-    <head><title>Slow Wave Audio</title><link rel="icon" href="../images/sw_icon-white.png"></link></head>
-      <img src={logo} className={styles.logo} />
-      <div className={styles.text}>
-        With more than 10 years experience working in the television and media
-        industries, we provide meticulous attention to detail on every job, from
-        the prep right through to delivery. With experience in single,
-        multi-camera and fixed rig location shoots, we strive to deliver top
-        quality, and in sync audio whatever the circumstances and pride
-        ourselves in end-to-end audio solutions for every situation.
-      </div>
-      <div className={styles.soundsGood}>
-        Sounds good?
-        <div>
-          <strong><a href = "mailto: joel@slowwaveaudio.tv">Get in touch</a></strong> to start talking about your next project
-          today.
+    <div>
+      <Helmet>
+        <title>Slow Wave Audio</title>
+        <link rel="icon" href={favicon} />
+      </Helmet>
+      <div className={styles.page}>
+        <img src={logo} className={styles.logo} />
+        <div className={styles.text}>
+          With more than 10 years experience working in the television and media
+          industries, we provide meticulous attention to detail on every job,
+          from the prep right through to delivery. With experience in single,
+          multi-camera and fixed rig location shoots, we strive to deliver top
+          quality, and in sync audio whatever the circumstances and pride
+          ourselves in end-to-end audio solutions for every situation.
         </div>
-        <img src={joel} className={styles.joelImage} />
+        <div className={styles.soundsGood}>
+          Sounds good?
+          <div>
+            <strong>
+              <a href="mailto: joel@slowwaveaudio.tv">Get in touch</a>
+            </strong>{" "}
+            to start talking about your next project today.
+          </div>
+          <img src={joel} className={styles.joelImage} />
+        </div>
       </div>
     </div>
   );
